94e5bceba03aca675e848ecf9ccdb80135589839
jay-to-the-dee <jay-to-the-dee@users.noreply.github.com>
2018-01-06 02:36:19 +0000

    Now asks for permission and starts service for tracking

---
+++ app/src/main/java/com/jonathandilks/dilksysfit/DilkSysGPSService.java
@@ -0,0 +1,93 @@
+package com.jonathandilks.dilksysfit;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Binder;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.location.Location;
+import android.location.LocationManager;
+import android.location.LocationListener;
+import android.widget.Toast;
+
+
+/**
+ * Created by jonathan on 04/01/18.
+ */
+
+public class DilkSysGPSService extends Service {
+    private final IBinder binder = new DilkSysGPSServiceBinder();
+    private MyLocationListener locationListener;
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return binder;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "Service Destroyed", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        handleCommand(intent);
+        // We want this service to continue running until it is explicitly
+        // stopped, so return sticky.
+
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new MyLocationListener();
+        try {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                    5,
+                    5,
+                    (LocationListener) locationListener);
+        } catch (SecurityException e) {
+            Log.d(getResources().getString(R.string.app_name), e.toString());
+        }
+
+        return START_STICKY;
+    }
+
+    private void handleCommand(Intent intent) {
+    }
+
+    private class DilkSysGPSServiceBinder extends Binder {
+        //Bindings
+    }
+
+    private class MyLocationListener implements LocationListener {
+        @Override
+        public void onLocationChanged(Location location) {
+            Log.d("g53mdp", location.getLatitude() + " " + location.getLongitude());
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+        // information about the signal, i.e. number of satellites
+            Log.d("g53mdp", "onStatusChanged: " + provider + " " + status);
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+        // the user enabled (for example) the GPS
+            Log.d("g53mdp", "onProviderEnabled: " + provider);
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+        // the user disabled (for example) the GPS
+            Log.d("g53mdp", "onProviderDisabled: " + provider);
+        }
+    }
+}
--- app/src/main/java/com/jonathandilks/dilksysfit/MainActivity.java
+++ app/src/main/java/com/jonathandilks/dilksysfit/MainActivity.java
@@ -1,15 +1,22 @@
 package com.jonathandilks.dilksysfit;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final int MY_PERMISSIONS_REQUEST_FINE_LOCATION = 1;
     private TextView mTextMessage;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
@@ -38,15 +45,56 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        BottomNavigationView navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        checkPermissionAndStartGPSservice();
     }
 
+    private void checkPermissionAndStartGPSservice() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    MainActivity.MY_PERMISSIONS_REQUEST_FINE_LOCATION);
+        }
+        else
+        {
+            startDilksGPSService();
+        }
+    }
+
+    private void startDilksGPSService() {
+        Intent intent = new Intent(this, DilkSysGPSService.class);
+        startService(intent);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_FINE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    //Sweet! We are good to go! Alles ist gut!
+
+                    startDilksGPSService();
+                } else {
+                    Toast.makeText(this, R.string.error_no_gps_permission, Toast.LENGTH_LONG).show();
+                    finish(); //Close the app as it won't work. User will see error message.
+                }
+                return;
+            }
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.gps_recording_menu, menu);
         return true;
     }
+
 
 }
